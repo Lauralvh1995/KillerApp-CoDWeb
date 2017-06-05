4be1a521bc722b98d351e1de5fdec438f29902b7
Laura van Helden <l.vanhelden@student.fontys.nl>
2017-06-05 14:40:00 +0200

    more

---
+++ WoDWebBuilder/Models/CharacterWoDViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WoDWebBuilder.Models
+{
+    public class CharacterWoDViewModel
+    {
+        public int CharacterID { get; set; }
+        public int WoDID { get; set; }
+    }
+}
